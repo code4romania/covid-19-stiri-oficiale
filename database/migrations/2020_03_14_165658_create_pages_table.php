61fade152fed9aef1e3f299d3ebd1b5bf0992ff9
Andrei Ioniță <hi@andrei.io>
2020-03-15 19:28:02 +0000

    Fix slug validation. Remove observer

--- database/migrations/2020_03_14_165658_create_pages_table.php
+++ database/migrations/2020_03_14_165658_create_pages_table.php
@@ -16,7 +16,7 @@ class CreatePagesTable extends Migration
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->text('content');
             $table->softDeletes();
             $table->timestamps();
