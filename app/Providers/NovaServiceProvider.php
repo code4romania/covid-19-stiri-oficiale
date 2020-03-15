cdc3ea4d6913c89d96e68a607d89d7cce9710efb
Andrei Ioniță <hi@andrei.io>
2020-03-15 10:58:28 +0000

    Add deployment (#15)
    * Add deployment
    
    * Adjust nova gate
    
    * Update .env.example

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -19,7 +19,6 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function boot()
     {
         parent::boot();
-
     }
 
     /**
@@ -30,9 +29,9 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function routes()
     {
         Nova::routes()
-                ->withAuthenticationRoutes()
-                ->withPasswordResetRoutes()
-                ->register();
+            ->withAuthenticationRoutes()
+            ->withPasswordResetRoutes()
+            ->register();
     }
 
     /**
@@ -45,9 +44,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return $user->exists;
         });
     }
 
