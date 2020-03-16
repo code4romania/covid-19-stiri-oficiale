b5589eb6991cd7026c08f310faa35a2c4966f24c
Andrei Ioniță <hi@andrei.io>
2020-03-16 12:42:30 +0000

    Add header notice (#36)

---
+++ resources/views/partials/notice.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-blue-100">
+    <p class="container py-3 text-xs text-center text-blue-500">
+        Pentru noi, cei de la Code for Romania Task Force, timpul este o prioritate. Pentru că ne-am concentrat ca informațiile să ajungă la voi cât mai repede, unele dintre funcționalități vor fi active treptat, în zilele următoare.
+    </p>
+</div>
