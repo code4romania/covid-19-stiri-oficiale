d805bca94355a803461ea2bedaf27ca5562a44a0
Andrei Ioniță <hi@andrei.io>
2020-03-17 13:05:31 +0000

    Add favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,14 @@
     {!! SEO::generate() !!}
 
     <link href="{{ asset(mix('app.css', 'assets')) }}" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/images/favicons/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('assets/images/favicons/manifest.json') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <meta name="msapplication-config" content="{{ asset('assets/images/favicons/browserconfig.xml') }}">
 </head>
 <body class="flex flex-col min-h-screen font-light">
     @include('partials.header')
