ef9e67a2596c7b41e2d3bf2f91e02337c0543947
Utwo <mihai.legat@gmail.com>
2020-03-18 11:36:22 +0200

    Add --prefer-offline --no-audit to npm ci

--- recipes/assets.php
+++ recipes/assets.php
@@ -5,7 +5,7 @@ namespace Deployer;
 desc('Build frontend assets locally');
 task('assets:build', function () {
     if (!test('[ -d public/assets ]')) {
-        run('{{bin/npm}} ci');
+        run('{{bin/npm}} ci --prefer-offline --no-audit');
         run('{{bin/npm}} run production');
     }
 })->local();
