85e3025790e6c004e0235b18a4d99c1fcca5e28f
Andrei Ioniță <hi@andrei.io>
2020-03-26 18:37:43 +0000

    Explicit rich-text word-break

--- tailwind.config.js
+++ tailwind.config.js
@@ -189,6 +189,7 @@ module.exports = {
             },
             richText: {
                 lineHeight: theme('lineHeight.relaxed'),
+                wordBreak: 'break-word',
                 '> * + *': {
                     marginTop: theme('spacing.5'),
                 },
