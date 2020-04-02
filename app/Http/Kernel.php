3c96b137336ac7bac6f83f4a9c40665e0514f96f
Andrei Ioniță <hi@andrei.io>
2020-04-02 20:07:10 +0100

    Move session related middlewares to separate group

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,11 +33,14 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'session' => [
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
