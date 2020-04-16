325debf4b4f58c2a34b2a27129c7c52f828c9dec
Andrei Ioniță <hi@andrei.io>
2020-04-16 10:58:54 +0100

    Update notice text

--- resources/views/partials/notice.blade.php
+++ resources/views/partials/notice.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-yellow-500">
     <p class="container py-3 font-normal text-center">
-        <a href="https://stirioficiale.ro/15-recomandari-privind-conduita-sociala-responsabila-in-prevenirea-raspandirii-coronavirus" class="inline-block underline hover:no-underline">
-            15 RECOMANDĂRI privind conduita socială responsabilă în prevenirea răspândirii coronavirus.
+        <a href="https://cetrebuiesafac.ro/" target="_blank" rel="noopener" class="inline-block underline hover:no-underline">
+            Află ce trebuie să faci în contextul pandemiei COVID-19
         </a>
     </p>
 </div>
