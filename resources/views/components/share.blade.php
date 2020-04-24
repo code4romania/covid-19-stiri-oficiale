0ac2480cbec7faadcadff324fcbe67cc8c9ee2d5
Andrei Ioniță <hi@andrei.io>
2020-04-24 21:33:30 +0100

    Fix share buttons label (#90)

--- resources/views/components/share.blade.php
+++ resources/views/components/share.blade.php
@@ -1,11 +1,12 @@
 <div class="flex items-center justify-between my-6 border-b">
     <div x-data="share">
-        <span class="mr-3">Share on</span>
+        <span class="mr-3">{{ __('share.share') }}</span>
 
         @foreach ($platforms as $platform)
             <button
                 @click="popup('{{ $url }}', '{{ $platform['uri'] }}')"
                 class="w-8 h-8 mx-1 my-2 rounded-full hover:opacity-75 focus:outline-none focus:shadow-outline"
+                aria-label="{{ __('share.platform', ['platform' => ucfirst($platform['id']) ]) }}"
             >
                 @svg("icons/{$platform['id']}", 'w-full h-full')
             </button>
