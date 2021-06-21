2369064162fdf7abf9e91e3b553b718825cac112
Andrei Ioniță <hi@andrei.io>
2021-06-21 12:19:17 +0100

    add tasks, remove recipes

---
+++ deploy/tasks/npm.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deployer;
+
+set('bin/npm', fn () => locateBinaryPath('npm'));
+
+desc('Install npm packages');
+task('npm:install', function () {
+    if (test('[ -d node_modules ]')) {
+        return;
+    }
+
+    run('{{bin/npm}} ci --prefer-offline');
+})->local();
+
+desc('Build frontend assets locally');
+task('npm:build', function () {
+    if (test('[ -d public/assets ]')) {
+        return;
+    }
+
+    run('{{bin/npm}} run production');
+})->local();
+
+desc('Upload assets to your hosts');
+task('npm:upload', function () {
+    upload('public/assets/', '{{release_path}}/public/assets/');
+});
